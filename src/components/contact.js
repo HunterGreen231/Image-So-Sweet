a76dea62f701a823f985d5179f2cea1563b819ac
g-hunter <webdev.huntergreen@gmail.com>
2019-06-13 20:15:42 -0600

    Added more styling to home page

--- src/components/contact.js
+++ src/components/contact.js
@@ -10,19 +10,54 @@ export default class Contact extends Component {
   render() {
     return (
       <div className="contact-wrapper">
-        <h1 style={{ paddingTop: "60px" }}>Contact Section</h1>
-        <form>
-          <input name="firstName" />
-          <input name="lastName" />
-          <input name="email" />
-          <input name="phoneNumber" />
-          <input name="highSchool" />
-          <input name="seniorYear" />
-          <input name="seniorName" />
-          <input name="referredBy" />
-          <textarea name="message" />
-          <button type="submit">Send Mail</button>
-        </form>
+        <div className="contact-background" />
+        <div className="contact-content">
+          <h1>Contact</h1>
+          <form className="form-wrapper">
+            <div className="three-column">
+              <input
+                name="firstName"
+                placeholder="First Name"
+                className="input-field"
+              />
+              <input
+                name="lastName"
+                placeholder="Last Name"
+                className="input-field"
+              />
+              <input name="email" placeholder="Email" className="input-field" />
+              <input
+                name="phoneNumber"
+                placeholder="Phone Number"
+                className="input-field"
+              />
+              <input
+                name="fantasyTheme"
+                placeholder="Fantasy Theme"
+                className="input-field"
+              />
+              <input
+                name="location"
+                placeholder="Location"
+                className="input-field"
+              />
+            </div>
+            <div className="two-column">
+              <input
+                name="childsName"
+                placeholder="Child's Name"
+                className="input-field"
+              />
+              <input
+                name="referredBy"
+                placeholder="Referred By"
+                className="input-field"
+              />
+            </div>
+            <textarea name="message" placeholder="Comments/Messages" />
+            <button type="submit">Send Mail</button>
+          </form>
+        </div>
       </div>
     );
   }
