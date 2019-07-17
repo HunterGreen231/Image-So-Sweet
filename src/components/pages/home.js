54065fc52610a9e20f5ff2d5f51405ecb1654616
g-hunter <webdev.huntergreen@gmail.com>
2019-07-16 18:35:21 -0600

    debugging...

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -18,7 +18,7 @@ export default class Home extends Component {
   }
 
   componentWillMount() {
-    console.log(process.env.IMAGES_API_URL);
+    console.log(process.env);
     console.log(process.env.NODE_ENV);
     axios
       .get(process.env.IMAGES_API_URL)
